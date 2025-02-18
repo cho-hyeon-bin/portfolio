6b0abd67db19beb54e5ba69ef46ebe8cfad68407
chohyeonbin <insideb94@gmail.com>
2025-02-18 20:21:07 +0900

    헤더 레이아웃

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,25 @@
+
 function Header() {
 	return (
-		<p>header</p>
+		<div>
+			<div>
+				<div className="logo">
+					<img src="/src/assets/image/logo.svg" alt="logo" />
+				</div>
+				<ul>
+					<li>Home</li>
+					<li>Project</li>
+					<li>Skill</li>
+				</ul>
+			</div>
+			<div>
+				<button type="button"></button>
+				<button type="button"></button>
+				<div>
+					<img src="/src/assets/image/profile-blue.png" alt="profile-blue" />
+				</div>
+			</div>
+		</div>
 	);
 }
 
