3b7cc847fd4b8bea1c35770033534d30e149571a
chohyeonbin <insideb94@gmail.com>
2025-05-16 14:38:05 +0900

    refactoring

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '@/components/Header.tsx';
-// import Footer from '@/components/Footer.tsx';
 import '@/sass/common/components/layout.sass';
 
 const Layout: React.FC = () => {
 	return (
 		<div className="layout">
-			<Header />
 			<main className="main-content">
 				<Outlet />
 			</main>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,32 +1,24 @@
-import { useEffect } from 'react';
-import gsap from 'gsap';
 import '@/sass/view/home.sass'
 const HomeComponent = () => {
-	useEffect(() => {
-		gsap.to('.portfolio', {
-			scale: 0.8,
-			duration: 1,
-			ease: 'power1.inOut',
-			yoyo: true,
-			repeat: -1
-		});
-	}, []);
 	return (
 		<section className="main-wrap">	
-			<div className="main">
-				<img src="/src/assets/image/portfolio_main.png" alt="portfolio_main" className="portfolio"/>
-				<img src="/src/assets/image/object3.png" alt="object3" className="frontend"/>
-				<img src="/src/assets/image/object1.png" alt="object1" className="purple"/>
-				<img src="/src/assets/image/object2.png" alt="object2" className="star"/>
-				<img src="/src/assets/image/object4.png" alt="object4" className="smile" />
-				<img src="/src/assets/image/object8.png" alt="object8" className="green"/>
-				<img src="/src/assets/image/object6.png" alt="object6" className="flower-y"/>
-				<img src="/src/assets/image/object5.png" alt="object5" className="rainbow1"/>
-				<img src="/src/assets/image/object7.png" alt="object7" className="flower-g" />
-				<img src="/src/assets/image/object9.png" alt="object9" className="rainbow2"/>
+			<div className="header-box">
+				<span>CHOHYEONBIN .</span>
+				<div>
+					<span>self service</span>
+					<span>20% off</span>
+				</div>
+				<div>
+					<ul>
+						<li>about me</li>
+						<li>projects</li>
+						<li>skill</li>
+						<li>contact</li>
+					</ul>
+				</div>
 			</div>
 		</section>
 	);
 };
 
-export default HomeComponent;
+export default HomeComponent
