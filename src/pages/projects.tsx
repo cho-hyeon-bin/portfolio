3c4f30adc48ee3d501b0e8f78d44e4fd5fc1cc5b
조현빈 <97923969+cho-hyeon-bin@users.noreply.github.com>
2025-09-03 21:59:41 +0900

    project

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,56 +1,183 @@
-import '@/sass/view/projects.sass'
+import "@/sass/view/projects.sass";
 const ProjectsComponent = () => {
-	return (
-		<section className="projects-container">
-			<div className="projects-wrap">
-				<div className="title">
-					<span>projects</span>
-				</div>
-				<div className="project-list">
-					<div className="project-box">
-						<div className="img-box">
-							<img src="/src/assets/image/marketbang-new.png" alt="onoffmix" />
-						</div>
-						<div className="discription-box">
-							<span>MAKETBANG</span>
-							<p>마켓뱅은 소매상 점주들이 모바일에서 와인을 쉽고 빠르게 발주할 수 있는 B2B 플랫폼입니다.<br/><br/>100여 개 수입사와 5,000여 개 상품을 연동하며, 실시간 재고 조회부터 공동구매까지 와인 유통에 최적화된 기능을 제공합니다.</p>
-							<button><span>see project</span></button>
-						</div>
-					</div>
-					<div className="project-box">
-						<div className="img-box">
-							<img src="/src/assets/image/koita-new.png" alt="onoffmix" />
-						</div>
-						<div className="discription-box">
-							<span>KOITA 기술경영인 하계포럼</span>
-							<p>국내 최대 규모의 기술경영 행사 ‘기술경영인 하계포럼’을 위한 신청·운영 시스템을 개발했습니다.<br/><br/>참가자와 관리자가 효율적으로 소통하고 정보를 관리할 수 있도록 지원하는 웹 기반 신청 시스템을 제공합니다.</p>
-							<button><span>see project</span></button>
-						</div>
-					</div>
-					<div className="project-box">
-						<div className="img-box">
-							<img src="/src/assets/image/ksa-new.png" alt="onoffmix" />
-						</div>
-						<div className="discription-box">
-							<span>KSA CEO Forum</span>
-							<p>70회를 넘긴 전통의 ‘KSA 하계 CEO 포럼’은 국내외 주요 인사들과 함께 정책과 경영을 논의하는 대표적인 경영자 교류 행사입니다.<br/><br/>포럼 참가자와 운영진을 위한 신청 및 운영 시스템을 개발하여 전 과정을 디지털화하고 효율성을 극대화하는 데 중점을 두었습니다.</p>
-							<button><span>see project</span></button>
-						</div>
-					</div>
-					<div className="project-box">
-						<div className="img-box">
-							<img src="/src/assets/image/pco-new.png" alt="onoffmix" />
-						</div>
-						<div className="discription-box">
-							<span>PROMICE</span>
-							<p>컨벤션 분야 진출을 희망하는 청년과 실습 인턴 채용을 원하는 기업을 연결하는 채용 및 취업 매칭 서비스입니다. <br/><br/>컨벤션 산업 전문 인력 육성과 실무 경험 기회 제공에 중점을 두었습니다.</p>
-							<button><span>see project</span></button>
-						</div>
-					</div>
-				</div>
-				<button type="button" className="more-btn"><span>show more projects</span></button>
+  return (
+	<section className="projects-container">
+	  <div className="projects-wrap">
+		<div className="title">
+		  <span>projects</span>
+		</div>
+		<div className="project-list">
+		  <div className="project-box">
+			<div className="img-box">
+			  <img src="/src/assets/image/marketbang-new.png" alt="onoffmix" />
 			</div>
-		</section>
-	)
-}
+			<div className="discription-box">
+				<span>MAKETBANG</span>
+			  	<div>
+					<button>vue3</button>
+					<button>javascript</button>
+					<button>sass</button>
+			  	</div>
+			  	<button>
+				<span>see project</span>
+			  </button>
+			</div>
+		  </div>
+		  <div className="project-box">
+			<div className="img-box">
+			  <img src="/src/assets/image/koita-new.png" alt="onoffmix" />
+			</div>
+			<div className="discription-box">
+				<span>KOITA 기술경영인 하계포럼</span>
+				<div>
+					<button>vue3</button>
+					<button>javascript</button>
+					<button>sass</button>
+			  	</div>			
+				<button>
+					<span>see project</span>
+				</button>
+			</div>
+		  </div>
+		  <div className="project-box">
+			<div className="img-box">
+			  <img src="/src/assets/image/ksa-new.png" alt="onoffmix" />
+			</div>
+			<div className="discription-box">
+			  <span>KSA CEO Forum</span>
+			  <p>
+				70회를 넘긴 전통의 ‘KSA 하계 CEO 포럼’은 국내외 주요 인사들과
+				함께 정책과 경영을 논의하는 대표적인 경영자 교류 행사입니다.
+				<br />
+				<br />
+				포럼 참가자와 운영진을 위한 신청 및 운영 시스템을 개발하여 전
+				과정을 디지털화하고 효율성을 극대화하는 데 중점을 두었습니다.
+			  </p>
+			  <button>
+				<span>see project</span>
+			  </button>
+			</div>
+		  </div>
+		  <div className="project-box">
+			<div className="img-box">
+			  <img src="/src/assets/image/pco-new.png" alt="onoffmix" />
+			</div>
+			<div className="discription-box">
+			  <span>PROMICE</span>
+			  <p>
+				컨벤션 분야 진출을 희망하는 청년과 실습 인턴 채용을 원하는
+				기업을 연결하는 채용 및 취업 매칭 서비스입니다. <br />
+				<br />
+				컨벤션 산업 전문 인력 육성과 실무 경험 기회 제공에 중점을
+				두었습니다.
+			  </p>
+			  <button>
+				<span>see project</span>
+			  </button>
+			</div>
+		  </div>
+		  <div className="project-box">
+			<div className="img-box">
+			  <img src="/src/assets/image/chungpung.png" alt="chungpung" />
+			</div>
+			<div className="discription-box">
+			  <span>청풍케이블카</span>
+			  <p>
+				컨벤션 분야 진출을 희망하는 청년과 실습 인턴 채용을 원하는
+				기업을 연결하는 채용 및 취업 매칭 서비스입니다. <br />
+				<br />
+				컨벤션 산업 전문 인력 육성과 실무 경험 기회 제공에 중점을
+				두었습니다.
+			  </p>
+			  <button>
+				<span>see project</span>
+			  </button>
+			</div>
+		  </div>
+		  <div className="project-box">
+			<div className="img-box">
+			  <img src="/src/assets/image/1004.png" alt="1004" />
+			</div>
+			<div className="discription-box">
+			  <span>1004요트</span>
+			  <p>
+				컨벤션 분야 진출을 희망하는 청년과 실습 인턴 채용을 원하는
+				기업을 연결하는 채용 및 취업 매칭 서비스입니다. <br />
+				<br />
+				컨벤션 산업 전문 인력 육성과 실무 경험 기회 제공에 중점을
+				두었습니다.
+			  </p>
+			  <button>
+				<span>see project</span>
+			  </button>
+			</div>
+		  </div>
+		  <div className="project-box">
+			<div className="img-box">
+			  <img src="/src/assets/image/jmconnected.png" alt="jmconnected" />
+			</div>
+			<div className="discription-box">
+			  <span>JM Connected</span>
+			  <p>
+				컨벤션 분야 진출을 희망하는 청년과 실습 인턴 채용을 원하는
+				기업을 연결하는 채용 및 취업 매칭 서비스입니다. <br />
+				<br />
+				컨벤션 산업 전문 인력 육성과 실무 경험 기회 제공에 중점을
+				두었습니다.
+			  </p>
+			  <button>
+				<span>see project</span>
+			  </button>
+			</div>
+		  </div>
+		  <div className="project-box">
+			<div className="img-box">
+			  <img
+				src="/src/assets/image/onoffmix-new.png"
+				alt="onoffmix-new"
+			  />
+			</div>
+			<div className="discription-box">
+			  <span>ONOFFMIX</span>
+			  <p>
+				컨벤션 분야 진출을 희망하는 청년과 실습 인턴 채용을 원하는
+				기업을 연결하는 채용 및 취업 매칭 서비스입니다. <br />
+				<br />
+				컨벤션 산업 전문 인력 육성과 실무 경험 기회 제공에 중점을
+				두었습니다.
+			  </p>
+			  <button>
+				<span>see project</span>
+			  </button>
+			</div>
+		  </div>
+		  <div className="project-box">
+			<div className="img-box">
+			  <img
+				src="/src/assets/image/onoffmix-new.png"
+				alt="onoffmix-new"
+			  />
+			</div>
+			<div className="discription-box">
+			  <span>Portfolio</span>
+			  <p>
+				컨벤션 분야 진출을 희망하는 청년과 실습 인턴 채용을 원하는
+				기업을 연결하는 채용 및 취업 매칭 서비스입니다. <br />
+				<br />
+				컨벤션 산업 전문 인력 육성과 실무 경험 기회 제공에 중점을
+				두었습니다.
+			  </p>
+			  <button>
+				<span>see project</span>
+			  </button>
+			</div>
+		  </div>
+		</div>
+		<button type="button" className="more-btn">
+		  <span>show more projects</span>
+		</button>
+	  </div>
+	</section>
+  );
+};
 export default ProjectsComponent;
--- src/pages/skill.tsx
+++ src/pages/skill.tsx
@@ -24,50 +24,71 @@ const SkillComponent = () => {
 					<div className="skill-box">
 						<img src="/src/assets/image/html-icon.svg" alt="html-icon" />
 						<div>
-							<p>html1</p>
+							<p>html5</p>
 							<p>Responsive websites with fast loading</p>
 						</div>
 					</div>
 					<div className="skill-box">
-						<img src="/src/assets/image/html-icon.svg" alt="html-icon" />
+						<img src="/src/assets/image/html-icon.svg" alt="vue-icon" />
 						<div>
-							<p>html1</p>
-							<p>Responsive websites with fast loading</p>
+							<p>Vue.js</p>
+							<p>컴포넌트 기반 설계, Vuex/Pinia 상태 관리</p>
 						</div>
 					</div>
 					<div className="skill-box">
-						<img src="/src/assets/image/html-icon.svg" alt="html-icon" />
+						<img src="/src/assets/image/html-icon.svg" alt="js-ts-icon" />
 						<div>
-							<p>html1</p>
-							<p>Responsive websites with fast loading</p>
+							<p>JavaScript & TypeScript</p>
+							<p>ES6+, async/await, 정적 타입 체크</p>
 						</div>
 					</div>
 					<div className="skill-box">
-						<img src="/src/assets/image/html-icon.svg" alt="html-icon" />
+						<img src="/src/assets/image/html-icon.svg" alt="react-icon" />
 						<div>
-							<p>html1</p>
-							<p>Responsive websites with fast loading</p>
+							<p>React.js</p>
+							<p>함수형 컴포넌트, 훅 기반 상태 관리, 리팩토링</p>
 						</div>
 					</div>
 					<div className="skill-box">
-						<img src="/src/assets/image/html-icon.svg" alt="html-icon" />
+						<img src="/src/assets/image/html-icon.svg" alt="jquery-icon" />
 						<div>
-							<p>html1</p>
-							<p>Responsive websites with fast loading</p>
+							<p>jQuery</p>
+							<p>Legacy 코드 유지보수 및 최적화</p>
 						</div>
 					</div>
 					<div className="skill-box">
-						<img src="/src/assets/image/html-icon.svg" alt="html-icon" />
+						<img src="/src/assets/image/html-icon.svg" alt="html-css-icon" />
 						<div>
-							<p>html1</p>
-							<p>Responsive websites with fast loading</p>
+							<p>CSS3</p>
+							<p>웹 표준, 반응형 레이아웃, 미디어 쿼리</p>
 						</div>
 					</div>
 					<div className="skill-box">
-						<img src="/src/assets/image/html-icon.svg" alt="html-icon" />
+						<img src="/src/assets/image/html-icon.svg" alt="sass-tailwind-icon" />
 						<div>
-							<p>html1</p>
-							<p>Responsive websites with fast loading</p>
+							<p>Sass / Tailwind CSS</p>
+							<p>유지보수성 높은 스타일링, 일관성 있는 UI</p>
+						</div>
+					</div>
+					<div className="skill-box">
+						<img src="/src/assets/image/html-icon.svg" alt="git-icon" />
+						<div>
+							<p>Git</p>
+							<p>버전 관리, 브랜치 전략, 충돌 해결</p>
+						</div>
+					</div>
+					<div className="skill-box">
+						<img src="/src/assets/image/html-icon.svg" alt="jira-icon" />
+						<div>
+							<p>Jira</p>
+							<p>애자일 스프린트 관리, 이슈 트래킹</p>
+						</div>
+					</div>
+					<div className="skill-box">
+						<img src="/src/assets/image/html-icon.svg" alt="confluence-icon" />
+						<div>
+							<p>Confluence</p>
+							<p>프로젝트 문서화, 지식 공유</p>
 						</div>
 					</div>
 					{/* <Slider {...settings}>
